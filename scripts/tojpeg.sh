90c2cba8ae3b8ff141610b965995a27db43936ad
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2022-09-19 23:10:35 -0300

    show tables

--- scripts/tojpeg.sh
+++ scripts/tojpeg.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-pnmtojpeg -qtable=data/qtable.txt $1 ${1/pgm/png} 
+# to see the qtables:
+pnmtojpeg -quality=30 $1 > ${1/pgm/_q30.jpg} 
+jpegtopnm -tracelevel 10 ${1/pgm/_q30.jpg} > /dev/null 2> /tmp/pepe.txt
+grep -A8 "Define Quanti" /tmp/pepe.txt | tail -n 8
+# to use a custom qtable
+# pnmtojpeg -qtable=data/qtable.txt $1 ${1/pgm/png} 
